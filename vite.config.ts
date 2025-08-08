41cadb8deba99cd9eaea379c342f0e7c71558c98
Hemanth Guvvala <hemanthguvvala.personal@gmail.com>
2025-08-09 00:08:57 +0530

    Deployment gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is the crucial line that fixes the CSS path
+  // This line is essential for GitHub Pages.
   base: '/intelligent-resume-builder/',
 })
